819dd7a739e9558f2c0457594720c5e1e99ad472
dgermano17 <danilo.germano@etec.sp.gov.br>
2019-05-21 21:34:12 -0300

    INICIAL

---
+++ src/psv/Conexao.java
@@ -0,0 +1,42 @@
+
+package psv;
+import  java.sql.*;
+import static java.sql.DriverManager.getConnection;
+
+public class Conexao {
+    
+    public static Connection abrirConexao(){
+        Connection con = null;
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            String url= "";
+            url += "jdbc:mysql://127.0.0.1/estacionamento?";
+            url += "user=root&password";
+            
+            con = DriverManager.getConnection(url);
+            System.out.println("Conexão aberta");
+        
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }catch (ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return con;
+        
+    }
+    
+    public static void fechatConexao(Connection con){
+        
+        try{
+            con.close();
+            System.out.println("Conexão fechada");
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
