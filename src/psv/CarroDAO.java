3d593470a7fb05fb840ddb6abb9d2bd726975e9a
dgermano <danilo.germano@etec.sp.gov.br>
2019-05-28 20:12:22 -0300

    CRUD 1 - continuação

--- src/psv/CarroDAO.java
+++ src/psv/CarroDAO.java
@@ -3,5 +3,30 @@ import java.sql.*;
 import java.util.*;
 
 public class CarroDAO {
+    private Connection con; 
+
+    public Connection getCon() {
+        return con;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
+    }
     
+    
+    public CarroDAO(Connection con){
+        setCon(con);
+    }       
+
+    public String inserir(CarroBean carro){
+        
+        String sql = "insert into carro(placa,cor,descricao) values(?,?,?)";
+        
+        try{
+            PrepareStatement ps = getCon().prepareStatement(sql);
+            ps.setString(1, carro.getPlaca());
+            ps.setString(2, carro.getCor());
+            ps.setString(3, carro.getDescricao());
+        }
+    }
 }
